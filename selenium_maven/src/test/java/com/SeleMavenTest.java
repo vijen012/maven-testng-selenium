a6ccf63f1bc9c7bebfa022f789ce1983c1670b13
vijen012 <vijender08malav@gmail.com>
2017-02-26 22:22:16 +0530

    commit new files

---
+++ selenium_maven/src/test/java/com/SeleMavenTest.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleMavenTest 
+{
+	@BeforeTest
+	public void beforeTest()
+	{
+		System.out.println("Before Test");
+	}
+	
+	@Test
+	public void testLaunchURL() 
+	{
+		System.out.println("Launch URL");
+	}
+	
+	@AfterTest
+	public void afterTest()
+	{
+		System.out.println("After Test");
+	}
+}
